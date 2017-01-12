d6777bc063fea9f0267dc83d4b9c81eb453567c1
Yura Myhaylyak <ghost86421@gmail.com>
2017-01-12 21:12:29 +0200

    handmade hover-dropdown list

--- imports/ui/main/main.js
+++ imports/ui/main/main.js
@@ -2,10 +2,6 @@ import { Template } from 'meteor/templating';
 
 import './mainLayout.html';
 
-Template.mainLayout.onRendered(function() {
-	this.$('.ui.dropdown').dropdown();
-});
-
 Template.mainLayout.events({
 	'click #logout-btn'(ev) {
 		ev.preventDefault();
