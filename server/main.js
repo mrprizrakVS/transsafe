9bfee105bbbceda859c8477d32919f31f8853c6f
Yura Myhaylyak <ghost86421@gmail.com>
2017-01-28 23:14:19 +0200

    Add basis of upload file

--- server/main.js
+++ server/main.js
@@ -1,3 +1,6 @@
 import '../imports/api/users.js';
+import '../imports/api/user.js';
+import '../imports/api/images.js';
 
-import '../imports/publish/users.js';
+import '../imports/publish/user.js';
+import '../imports/publish/images.js';
