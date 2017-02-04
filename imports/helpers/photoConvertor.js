cbcee89f11551d1108e453af05a2591971bd07b3
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-04 12:23:23 +0200

    add default photo

--- imports/helpers/photoConvertor.js
+++ imports/helpers/photoConvertor.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 
-Template.registerHelper('photoConvertor', (path) => {
+Template.registerHelper('photoConvertor', (path = Meteor.settings.public.DEFAULT_PROFILE_PHOTO) => {
+  console.log('path', path);
   return path ? Meteor.settings.public.PHOTO_PATH.concat(path.split('/').slice(3).join('/')) : null;
 });
