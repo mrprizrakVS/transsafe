9d758dc1ee5c8aee323e540f74b3b980a6caf1e8
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-09 23:59:04 +0200

    Add update schema

---
+++ imports/schemas/serviceProfileSchema.js
@@ -0,0 +1,124 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+import '../config/schemas.config.js';
+
+const Schema = {};
+
+SimpleSchema.debug = true;
+
+Schema.contactInfo = new SimpleSchema({
+  contactPerson: {
+    type: String,
+    label: "Контактна особа",
+    autoform: {
+      placeholder: "Василь",
+      autocomplete: false
+    }
+  },
+  companySite: {
+    type: String,
+    label: "Сайт компанії",
+    optional: true,
+    autoform: {
+      placeholder: "example.ua"
+    }
+  }
+});
+
+Schema.generalInfo = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Назва",
+    autoform: {
+      placeholder: "Гарант-Автотехнік",
+      autocomplete: false
+    }
+  },
+  compType: {
+    type: String,
+    allowedValues: ['Компанія, що надає послуги'],
+    label: "Тип компанії",
+    autoform: {
+      firstOption: "- Оберіть тип -"
+    }
+  },
+  address: {
+    type: String,
+    label: "Адреса",
+    autoform: {
+      placeholder: "вул. Чорновола 15а",
+      autocomplete: false
+    }
+  },
+  phone: {
+    type: String,
+    label: "Телефон",
+    autoform: {
+      placeholder: "Номер"
+    }
+  },
+  numberOfWorker: {
+    type: String,
+    optional: true,
+    label: "К-сть робітників",
+    autoform: {
+      placeholder: "12",
+      autocomplete: false
+    }
+  }
+});
+
+Schema.ServiceProfile = new SimpleSchema({
+  generalInfo: {
+    type: Schema.generalInfo,
+    label: "Загальна інформація"
+  },
+  contactInfo: {
+    type: Schema.contactInfo,
+    label: "Контакна інформація"
+  },
+  role: {
+    type: String,
+    optional: true,
+    autoform: {
+      type: 'hidden'
+    }
+  }
+});
+
+
+Schema.ServiceSchema = new SimpleSchema({
+  profile: {
+    type: Schema.ServiceProfile,
+    label: 'Профіль'
+  },
+  email: {
+    type: String,
+    label: "Email адрес",
+    regEx: SimpleSchema.RegEx.Email,
+    autoform: {
+      placeholder: "example@gmail.com"
+    }
+  },
+  createdAt: {
+    type: Date,
+    optional: true,
+    autoValue() {
+      return new Date();
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  password: {
+    type: String,
+    label: "Пароль",
+    autoform: {
+       type: "password",
+       autocomplete: false,
+       placeholder: "Пароль"
+     }
+  }
+});
+
+export const ServiceProfileSchema = Schema.ServiceSchema;
--- imports/ui/serviceAccountEdit/serviceAccountEdit.js
+++ imports/ui/serviceAccountEdit/serviceAccountEdit.js
@@ -1,3 +1,24 @@
 import { Template } from 'meteor/templating';
 
+import { ServiceProfileSchema } from '../../schemas/serviceProfileSchema.js';
+import { User } from '../../api/user.js';
+
 import './serviceAccountEditLayout.html';
+
+Template.serviceAccountEditLayout.onCreated(function() {
+  this.autorun(() => {
+    Meteor.subscribe('user', FlowRouter.getParam('id'));
+  });
+});
+
+Template.serviceAccountEditLayout.helpers({
+  ServiceProfileSchema,
+  serviceDoc() {
+    let service = User.findOne();
+
+    service ? service.email = service.emails[0].address : null;
+
+    return service;
+  }
+});
+
