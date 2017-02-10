314d38b1a539ce07986f5fc549e38fdf220dcaca
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-11 01:20:59 +0200

    Add addition-photo component

---
+++ imports/components/addition_photos/addition_photos.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { Uploader } from '../../services/file_uploader.js';
+import { ImagesCollections } from '../../api/images.js';
+
+import './addition_photos.html';
+
+Template.AdditionPhotosLayout.onCreated(function() {
+  this.autorun(() => {
+    Meteor.subscribe('files.service.images.all');
+  });
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.AdditionPhotosLayout.helpers({
+  additionHelpers() {
+    return ImagesCollections.ServicesAdditionPhoto.find({
+      userId: FlowRouter.getParam('id')
+    });
+  }
+});
+
+Template.AdditionPhotosLayout.events({
+  'change #serviceUploadAdditionPhoto'(e, template) {
+     Uploader(e, template, 'ServicesAdditionPhoto');
+  }
+});
