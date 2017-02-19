a24cedbb6172fc04eacf42b92a4d65dae97898a0
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-19 16:14:24 +0200

    Add service gallery

--- imports/ui/serviceAccountPhotos/serviceAccountPhotos.js
+++ imports/ui/serviceAccountPhotos/serviceAccountPhotos.js
@@ -1,10 +1,28 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { ImagesCollections } from '../../api/images.js';
 
 import './serviceAccountPhotosLayout.html';
 
+Template.serviceAccountPhotosLayout.onRendered(() => {
+  $('.swipebox').swipebox();
+});
+
+Template.serviceAccountPhotosLayout.onCreated(function() {
+  this.autorun(() => {
+    Meteor.subscribe('files.service.images.all');
+  });
+});
+
+
 
 Template.serviceAccountPhotosLayout.helpers({
-  title() {
-    return 'Hello'
+  photos() {
+    return ImagesCollections.ServicesAdditionPhoto.find({
+      userId: FlowRouter.getParam('id')
+    }, {
+      sort: { 'meta.date': -1 },
+    });
   }
 });
