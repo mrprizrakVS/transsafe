2380d646720e5f0e261a65bd5b734fa64b28c6a8
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-25 10:37:55 +0200

    Add rules to collections

--- imports/api/user.cars.js
+++ imports/api/user.cars.js
@@ -5,5 +5,18 @@ import { UserCarsSchema } from '../schemas/userCarsSchema.js';
 const UserCars = new Mongo.Collection('userCars');
 UserCars.attachSchema(UserCarsSchema);
 
+// Allow rules for collection
+UserCars.allow({
+  insert(userId, doc) {
+    return userId === doc.ownerId;
+  },
+  update(userId, doc) {
+    return userId === doc.ownerId;
+  },
+  remove(userId, doc) {
+    return userId === doc.ownerId;
+  }
+});
+
 
 export { UserCars };
