a3b8e20c9e8c6a22144fb45e454306ba5726330b
Yura Myhaylyak <ghost86421@gmail.com>
2017-02-25 16:06:12 +0200

    Add limit to news

--- imports/server/publish/user.news.subscribe.js
+++ imports/server/publish/user.news.subscribe.js
@@ -3,5 +3,7 @@ import { News } from '../../api/news.js';
 import { User } from '../../api/user.js';
 
 Meteor.publish('news.user.subscribe', (_id) => {
-  return News.find({authourId: {$in: User.findOne({_id}).profile.subscribeServices}});
+  return News.find({authourId: {$in: User.findOne({_id}).profile.subscribeServices}}, {
+    limit: 7
+  });
 });
